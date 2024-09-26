751b9591b8b75233f91893341cf5552c3ec5de63
tjaloc <k.locke@gmx.de>
2024-09-26 15:55:21 +0200

    Update photo_slider_puzzle.py
    - add comments
    - add old texture removal

--- Photo-Slider-Puzzle/photo_slider_puzzle.py
+++ Photo-Slider-Puzzle/photo_slider_puzzle.py
@@ -13,17 +13,19 @@ import glob
 
 DARK = '#303030'
 LIGHT = '#ffffff'
-BORDER_W = 20 if min(get_screen_size()) >= 600 else 10
+BORDER_W = 20 if min(get_screen_size()) >= 600 else 10 # breakpoint for iPad/iPhone
 SIZE = 3
 FONT = ('<System-Bold>', 20)
-
+IMG_DIR = 'img'
 
 class Puzzle(Scene):
     def setup(self):
+        # Status
         self.initialized = False
         self.solved = False
         self.muted = False
-        
+
+        # Dimensions
         self.w, self.h = get_screen_size()
         self.puzzle_w = int(0.7 * min(self.w, self.h))
         self.tile_w = self.puzzle_w // SIZE
@@ -44,14 +46,16 @@ class Puzzle(Scene):
                 BORDER_W
                 ),
             )
-        
+
+        # Sound Button
         self.mute_btn = SpriteNode(
             'typw:Mute', 
             parent=self.root_node, 
             position=(40, self.h - 40),
             alpha=.2,
             )
-            
+
+        # Refresh Button
         self.new_puzzle_btn = SpriteNode(
             'typw:Refresh',
             position=(self.w/2, self.h/2 - self.puzzle_w/2 - 60),
@@ -60,25 +64,38 @@ class Puzzle(Scene):
             )
         
         self.new_puzzle()
+
+    def save_texture(self, texture):
+        """ Save tile texture under random name. This ensures no old texture will be loaded from cache.
+        Player get a new photo each game.
+        """
+        if not os.path.exists(IMG_DIR):
+            os.mkdir(IMG_DIR)
+        filename = f'img/{random.randint(1, 1_000_000):0>7}.png'
+        texture.save(filename)
+
+        return filename
         
     def get_image(self):
+        """Download photo from https://picsum.photos, slice it to tile size.
+        Create tile nodes and store as property tile_list.
+        """
         r = requests.get(f'https://picsum.photos/{self.puzzle_w}/{self.puzzle_w}?greyscale')
         if r.status_code == 200:
             img = Image.open(BytesIO(r.content))
             img.load()
             
-            for row in range( SIZE):
-                for col in range( SIZE):
+            for row in range(SIZE):
+                for col in range(SIZE):
                     nr = row * SIZE + col
                     
                     x0 = col * self.tile_w
                     x1 = x0 + self.tile_w
                     y0 = row * self.tile_w
                     y1 = y0 + self.tile_w
-                    
+
                     texture = img.crop((x0, y0, x1, y1))
-                    texture_filename = f'img/{random.randint(1, 1_000_000):0>7}.png'
-                    texture.save(texture_filename)
+                    texture_filename = self.save_texture(texture)
                     
                     rect = ui.Path.rounded_rect(0, 0, self.tile_w, self.tile_w, BORDER_W/2)
                     tile = ShapeNode(
@@ -86,7 +103,7 @@ class Puzzle(Scene):
                         fill_color=None,
                         stroke_color=LIGHT,
                         parent=self.puzzle_frame,
-                        alpha=nr > 0, # tile 0 is empty with alpha = 0
+                        alpha=nr > 0, # tile 0 is empty / invisible with alpha = 0
                         position=(self.tile_w/2, self.tile_w/2),
                         )
                         
@@ -99,14 +116,20 @@ class Puzzle(Scene):
                     self.tile_list.append(tile)
         
     def shuffle_puzzle(self):
+        """ Perform moves to shuffle the board.
+            Why not random? The smaller the board the higher the risk of unsolveability.
+        """
         neighbours = [ +1, -1, + SIZE, - SIZE]
-        for _ in range(SIZE ** 2):
+        for _ in range(SIZE ** 2 * 2):
             neighbour = random.choice(
                 [tile for tile in self.tile_list if any(
                     tile.cell + n == self.zero.cell for n in neighbours)])
             self.zero.cell, neighbour.cell = neighbour.cell, self.zero.cell
             
     def place_tiles(self):
+        """Place tiles on board.
+        Starting point is top left.
+        """
         for tile in self.tile_list:
             r, c = divmod(tile.cell, SIZE)
             x = self.start_x + c * self.tile_w
@@ -114,12 +137,19 @@ class Puzzle(Scene):
             tile.position = (x, y)
         
     def touched_tile(self, touch):
+        """Match touch location to tiles and return tile (node).
+        """
         for nr, tile in enumerate(self.tile_list):
             xy = self.puzzle_frame.point_from_scene(touch.location)
             if tile.frame.contains_point(xy):
                 return tile
             
     def touch_ended(self, touch):
+        """Perform action connected to touched node.
+        mute_btn:       switch Sound on / off
+        new_puzzle_btn: start new game
+        touched_tile:   move tile to empty cell
+        """
         # mute toggle
         if self.mute_btn.frame.contains_point(touch.location):
             self.muted = not self.muted
@@ -144,6 +174,8 @@ class Puzzle(Scene):
             return
             
     def is_neighbour(self, tile):
+        """return True if tile is direct neighbour of empty cell
+        """
         return any([
             tile.cell == self.zero.cell + 1, 
             tile.cell == self.zero.cell - 1, 
@@ -152,7 +184,11 @@ class Puzzle(Scene):
             ])
         
     def update(self):
-        if not self.initialized: return
+        """update Scene if game is initialized.
+        Stop game if puzzle is solved.
+        """
+        if not self.initialized:
+            return
         
         self.place_tiles()
         
@@ -162,16 +198,21 @@ class Puzzle(Scene):
             self.solved = True
     
     def puzzle_solved(self):
+        """Return True if all tiles are in their original cell"""
         return all(i == tile.cell for i, tile in enumerate(self.tile_list))
         
     def refresh(self):
+        """ Remove tile nodes from parent to avoid duplicates, old tiles in new game.
+        """
         for tile in self.tile_list:
             tile.remove_from_parent()
         self.new_puzzle()
             
     def new_puzzle(self):
+        """Set up for new game. Delete old stuff. Reset all status, create, shuffle and place new tiles
+        """
         # delete old textures
-        textures = glob.glob(os.path.join('img','*.png'))
+        textures = glob.glob(os.path.join('img', '*.png'))
         for texture in textures:
             os.remove(texture)
             
